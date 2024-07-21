83327bd668ae901d47aae0ebbf1315164cc9a1b3
Murat Polat <bomch4nte@gmail.com>
2024-07-21 18:28:51 +0300

    jwt helper funcs have been added.

---
+++ v1/src/helpers/jwt.ts
@@ -0,0 +1,43 @@
+import jwt from 'jsonwebtoken';
+import { response as resp } from '../constants/index';
+import { ServerError, Forbidden } from '../constants/errors';
+import { NextFunction } from 'express';
+
+export const create = (data, options) => {
+    try {
+        return jwt.sign(data, process.env.JWT_SECRET, options);
+    } catch (error) {
+        console.error(error);
+    }
+    return false;
+};
+
+/**
+ * jwt token verify and decode
+ * 
+ * @param {String} token 
+ * @returns 
+ */
+
+export const verify = (token) => {
+    try {
+        return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+        console.error(error);
+        if (error instanceof jwt.TokenExpiredError) throw new Forbidden('helpers.jwt.verify', 'token expired!');
+        if (error instanceof jwt.JsonWebTokenError) throw new Forbidden('helpers.jwt.verify', 'token error !');
+        throw new ServerError('helpers.jwt.verify', 'server error !');
+    }
+};
+
+export const create_with_refresh = (data, refresh_data, options) => {
+    try {
+        return {
+            token: jwt.sign(data, process.env.JWT_SECRET_KEY, options),
+            refresh_token: jwt.sign(refresh_data, process.env.JWT_SECRET_KEY, options)
+        };
+    } catch (error) {
+        console.error(error);
+    }
+    return false;
+};
