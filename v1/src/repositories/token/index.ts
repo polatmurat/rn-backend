ab81ab8e6a6c6e17e2b171ace280c217b05040de
Murat Polat <bomch4nte@gmail.com>
2024-07-22 11:51:48 +0300

    repository err

--- v1/src/repositories/token/index.ts
+++ v1/src/repositories/token/index.ts
@@ -3,10 +3,6 @@ import { MySql } from '../../db/MySql'
 
 const save = async (user_id: string, token_id: string, token: string, refresh_token: string, expire_at: string, type: string) => {
     try {
-        const expire_in = helpers.moment.moment().add(expire_at, 'seconds').format('X');
-        await MySql.execute('INSERT INTO user_tokens (type, user_id, token_id, token, refresh_token, expire_at, status) VALUES (?, ?, ?, ?, ?, ?, ?)',
-            [type, user_id, token_id, token, refresh_token, expire_in, true]
-        );
         return true;
     } catch (error) {
         console.error(error);
