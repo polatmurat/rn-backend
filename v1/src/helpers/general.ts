fcf224d78d227ff579bd8337bbae021f90de26a3
Murat Polat <bomch4nte@gmail.com>
2024-07-23 11:44:15 +0300

    exporting funcs (es6)

--- v1/src/helpers/general.ts
+++ v1/src/helpers/general.ts
@@ -1,18 +1,16 @@
-const validateEmail = (email: string): boolean => {
+export const validateEmail = (email: string): boolean => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
 };
 
-const getRandomNumber = (min: number, max: number): number => {
+export const getRandomNumber = (min: number, max: number): number => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const toBase64 = (string: string): string => {
+export const toBase64 = (string: string): string => {
     return Buffer.from(`${string}`).toString();
 };
 
-const fromBase64 = (base64: WithImplicitCoercion<string> | { [Symbol.toPrimitive](hint: "string"): string; }): string => {
+export const fromBase64 = (base64: WithImplicitCoercion<string> | { [Symbol.toPrimitive](hint: "string"): string; }): string => {
     return Buffer.from(base64, 'base64').toString();
 };
-
-module.exports = { validateEmail, getRandomNumber, toBase64, fromBase64 };
