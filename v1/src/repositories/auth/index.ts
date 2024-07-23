7faa13c2402ce53f31bbcd174cc4ab2de884ee60
Murat Polat <bomch4nte@gmail.com>
2024-07-23 15:23:00 +0300

    converting mysql.execute to pool.execute (repo, queries)

--- v1/src/repositories/auth/index.ts
+++ v1/src/repositories/auth/index.ts
@@ -1,12 +1,12 @@
-import { MySql } from '../../db/MySql';
+import { pool } from '../../db/MySql';
 import { NotFound, ServerError } from '../../constants/errors';
 
-export const loginRepo = async (email: string, password: string) => {
+
+export const loginRepo = async (email: string) => {
     try {
-        const data = await MySql.execute('SELECT * FROM users WHERE email=? AND password=? LIMIT 1', [email, password]);
-        if (Array.isArray(data) && data.length > 0) {
-            return { success: true, data };
-        }
+        const [data] = await pool.query('SELECT * FROM users WHERE email=? LIMIT 1', [email]);
+
+        if (Array.isArray(data) && data.length > 0) return { success: true, data };
     } catch (error) {
         console.error(error);
         throw new ServerError('repositories.users.login', 'repos error !');
@@ -15,10 +15,10 @@ export const loginRepo = async (email: string, password: string) => {
     throw new NotFound('repositories.auth.login', 'user not found !');
 };
 
-export const registerRepo = async (username: string, email: string, password: string, name: string) => {
+export const registerRepo = async (username: string, email: string, password: String, name: string) => {
     try {
-        const data = await MySql.execute('INSERT INTO users (username, email, password, name) VALUES (?,?,?,?)', [username, email, password, name]);
-        return data;
+        const [data] = await pool.query('INSERT INTO users (username, email, password, name) VALUES (?,?,?,?)', [username, email, password, name]);
+        if (Array.isArray(data) && data.length > 0) return { success: true, data };
     } catch (error) {
         console.error(error);
         throw new ServerError('repositories.auth.register', 'repos error !');
@@ -27,10 +27,10 @@ export const registerRepo = async (username: string, email: string, password: st
 
 export const checkRepo = async (username: string, email: string) => {
     try {
-        const datausername = await MySql.execute('SELECT username FROM users WHERE username=?', [username]);
+        const [datausername] = await pool.query('SELECT username FROM users WHERE username=?', [username]);
         if (Array.isArray(datausername) && datausername.length > 0) return { status: false, path: 'username' };
 
-        const dataemail = await MySql.execute('SELECT email FROM users WHERE email=?', [email]);
+        const [dataemail] = await pool.query('SELECT email FROM users WHERE email=?', [email]);
         if (Array.isArray(dataemail) && dataemail.length > 0) return { status: false, path: 'email' };
 
         return { status: true };
